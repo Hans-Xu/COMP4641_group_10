2c8d1fe7d558d3f915c590871fab9a1938b89849
XU Muyu <mxuai@connect.ust.hk>
2020-04-05 17:35:39 +0800

    update rumorDetector.py
    new arg: search_threshold
    
    now only when the text contains enough keywords will it be extracted.

--- rumorDetector.py
+++ rumorDetector.py
@@ -4,46 +4,64 @@ and store the result in a new csv
 '''
 
 import argparse
+import sys
 import os
 import pandas as pd
+import numpy as np
+from langdetect import detect
 
 # This function will read csv file 
 # and extract all lines whose text contains key words
 # then save to the distination path
-def findRumor(source_path: str, keywords: str, destination: str):
+def findRumor(source_path: str, keywords, destination: str, threshold: int):
     raw_data = pd.read_csv(source_path, dtype=str)
-    rumor_data = raw_data[raw_data['text'].str.contains(keywords)]
+    # extract all the text&quoted text and check the number of keywords they contain
+    text = raw_data['text']
+    quoted_text = raw_data['quoted_text']
+    valid_text = checkThreshold(text, threshold, keywords)
+    valid_quoted_text = checkThreshold(quoted_text, threshold, keywords)
+    # extract all the satisfied rows
+    rumor_data = raw_data[np.logical_or(valid_text,valid_quoted_text)]
+    # extract all the text&quoted text in the rumor data and check whether they are in English
+    '''
+    rumor_text = rumor_data['text']
+    rumor_quoted_text = rumor_data['quoted_text']
+    en_rumor = checkEnglish(rumor_text) & checkEnglish(rumor_quoted_text)
+    # extract the english rumor
+    rumor_data = rumor_data[en_rumor]
+    '''
+    # save data
     rumor_data.to_csv(destination, date_format='%s', index=False)  
     print('Work done!')    
 
-'''
-# This function can tranlate all content in the list to English
-def translate2En(original_text):
-    result = []
-    illegal_count = 0
-    total = len(original_text)
-    print('total: '+str(total))
-    count = 0.0
-    print_threshold = 0
-    for t in original_text:
-        try:
-            trans = Translator()
+# This function return the boolean form of the text list indicating the number of keywords in the text is over the threshold
+def checkThreshold(text_list, threshold, keywords):
+    boolean_list = np.empty([len(text_list), 1], dtype = bool)
+    for i in range(len(text_list)):
+        if countKeyWords(text_list[i], keywords) >= threshold:
+            boolean_list[i] = True
+        else:
+            boolean_list[i] = False
+    return boolean_list
+        
+# This function count the number of keywords that the text contains
+def countKeyWords(text, keywords): 
+    count = 0
+    text = str(text)
+    for word in keywords:
+        if word in text:
             count += 1
-            if (count/total*10000) > print_threshold:
-                print('process rate: '+ str(print_threshold)+'/10000')
-                print('current count: '+str(count))
-                print('current illegal: '+str(illegal_count))
-                print_threshold += 1
-            temp_res = trans.translate(t)
-        except Exception as e:
-            illegal_count += 1
-            temp_res = t
-        finally:
-            result.append(temp_res)
-    print(illegal_count)
-    print(str(len(original_text)))
-    return result
-'''
+    return count
+
+# This function return the boolean form of the text list indicating whether the text is English
+def checkEnglish(text_list):
+    boolean_list = np.empty([len(text_list), 1], dtype = bool)
+    for i in range(len(text_list)):
+        if detect(str(text_list[i]))=='en':
+            boolean_list[i] = True
+        else:
+            boolean_list[i] = False
+    return boolean_list
 
 if __name__ == "__main__":
     # Create our Argument parser and set its description
@@ -55,6 +73,7 @@ if __name__ == "__main__":
     #   - dest_file: the destination where the output should go
     #   - rumor_label: the name/label of this rumor
     #   - key_words: the words that will be used to search in text
+    #   - search_threshold: the threshold of the number of keywords in a text that should be extracted
 
     parser.add_argument(
         '-source_file',
@@ -78,18 +97,31 @@ if __name__ == "__main__":
         help='All keywords you want to search, use space to separate each word',
         nargs = '+'
     )
+
+    parser.add_argument(
+        '-search_threshold',
+        help='Only when the content contains more than or equal to this threshold will it be extracted',
+        default = 1,
+        type = int
+    )
     
-    # Parse the args (argparse automatically grabs the values from
-    # sys.argv)
+    # parse the args
     args = parser.parse_args()
 
     s_file = args.source_file
     d_file = args.dest_file
     r_label = args.rumor_label
     keywords = args.key_words
+    threshold = args.search_threshold
+    print(keywords)
+
+    # check whether the threshold is illegal
+    if threshold > len(keywords):
+        print('The threshold can not be larger than the number of keywords!')
+        sys.exit(0)
     
     # If the destination file wasn't passed, then assume we want to
-    # create a new file based on the old one
+    # create a new file 
     # If the file already exists, delete it and create a new one
     if d_file is None:
         file_path, file_extension = os.path.splitext(s_file)
@@ -100,13 +132,8 @@ if __name__ == "__main__":
     else:
         d_file = f'{d_file}'
     
-    
-    # Change the form of keywords for the input requirement of Pandas.Series.str.contains
-    k_words = keywords[0]
-    for i in range(1, len(keywords)):
-        k_words = k_words+'|'+keywords[i]
 
     # Search keywords and extract the information
-    findRumor(s_file, k_words, d_file)
+    findRumor(s_file, keywords, d_file, threshold)
 
     
